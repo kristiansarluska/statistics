7ed2d6271584d392c6f8dba97ed642cd77d6165f
kristiansarluska <kikosarluska@gmail.com>
2025-09-10 16:09:18 +0200

    First simple change of a website

---
+++ src/PrvyKomponent.jsx
@@ -0,0 +1,14 @@
+//PascalCasing
+function PrvyKomponent() {
+  //JSX
+  const name = "Kiko";
+  return (
+    <>
+      <h1>Hello {name}!</h1>
+      <p>tu je druhy element Fragmentu</p>
+      <p>tu je treti element Fragmentu</p>
+    </>
+  );
+}
+
+export default PrvyKomponent;
